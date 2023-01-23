bccab3c2294a85085c2110ec7ea1b4b2f717d3cb
Minh Hieu HOANG <hoang.minhhieu7693@gmail.com>
2023-01-23 19:44:27 +0100

    Init Quasar

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,9 @@
-/// <reference types="vite/client" />
+/* eslint-disable */
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: string;
+    VUE_ROUTER_MODE: 'hash' | 'history' | 'abstract' | undefined;
+    VUE_ROUTER_BASE: string | undefined;
+  }
+}
