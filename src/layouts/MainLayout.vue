821755550cc0f756871bccde64e9ba5c6d49f1f4
Minh Hieu HOANG <hoang.minhhieu7693@gmail.com>
2023-01-24 20:29:41 +0100

    Update header

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,9 +1,10 @@
 <!-- Notice lang="scss" -->
 <style lang="scss">
-.q-header {
+.header {
   display: flex;
   justify-content: center;
   background-color: black;
+  height: 60px;
 }
 
 .accountBtn {
@@ -11,47 +12,65 @@
 }
 
 .searchBar {
-  background-color: white;
   margin: 10px;
 }
 </style>
 
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-tabs v-model="tab">
-        <q-tab name="images" label="Khuyến mãi" />
-        <q-tab name="videos" label="Mã giảm giá" />
-        <q-tab name="articles" label="Miễn phí" />
-      </q-tabs>
+  <q-layout view="hHh lpR fFf">
+    <q-header class="bg-primary text-white">
+      <div class="header">
+        <q-tabs>
+          <q-tab name="images" label="Khuyến mãi" />
+          <q-tab name="videos" label="Mã giảm giá" />
+          <q-tab name="articles" label="Miễn phí" />
+        </q-tabs>
 
-      <q-input
-        standout
-        v-model="text"
-        placeholder="Tìm kiếm"
-        :class="'searchBar'"
-      >
-        <template v-slot:prepend>
-          <q-icon name="search" color="gray" />
-        </template>
-        <template v-slot:append>
-          <q-icon
-            name="close"
-            @click="text = ''"
-            class="cursor-pointer"
-            color="gray"
-          />
-        </template>
-      </q-input>
+        <q-input
+          v-model="search"
+          debounce="500"
+          outlined
+          color="navy"
+          bg-color="white"
+          rounded
+          placeholder="Tìm kiếm"
+          :class="'searchBar'"
+          dense
+        >
+          <template v-slot:prepend>
+            <q-icon name="search" color="gray" />
+          </template>
+          <template v-slot:append>
+            <q-icon
+              name="close"
+              @click="search = ''"
+              class="cursor-pointer"
+              color="gray"
+            />
+          </template>
+        </q-input>
 
-      <q-btn :class="'accountBtn'" color="grey">
-        <q-icon name="person" />
-        <div>Đăng nhập</div>
-      </q-btn>
+        <q-btn :class="'accountBtn'" color="blue">
+          <q-icon name="person" />
+          <div>Đăng nhập</div>
+        </q-btn>
+      </div>
     </q-header>
+
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-footer class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+          </q-avatar>
+          <div>Title</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
   </q-layout>
 </template>
 
@@ -65,8 +84,7 @@ export default defineComponent({
 
   setup() {
     return {
-      tab: ref('images'),
-      text: ref(''),
+      search: ref(''),
     };
   },
 });
